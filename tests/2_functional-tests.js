8a4a7649a3ae42b083b0c1e22186ea127fe72f0a
susana morello <morello.su@gmail.com>
2025-01-07 13:52:17 +0000

    Fixed unit tests and added functional tests with Zombie.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -76,7 +76,7 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.site = 'http://0.0.0.0:3000'
+Browser.site = 'https://3000-freecodecam-boilerplate-wo52zum7gwf.ws-eu117.gitpod.io'
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
@@ -97,10 +97,11 @@ suite('Functional Tests with Zombie.js', function () {
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       browser.fill('surname', 'Colombo').then(() => {
         browser.pressButton('submit', () => {
-          browser.assert.status(200);
+          browser.assert.success();
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.elements('span#dates', 1);
+          assert.isNotNull(browser.site);
           done();
         });
       });
@@ -110,10 +111,11 @@ suite('Functional Tests with Zombie.js', function () {
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       browser.fill('surname', 'Vespucci').then(() => {
         browser.pressButton('submit', () => {
-          browser.assert.status(200);
+          browser.assert.success();
           browser.assert.text('span#name', 'Amerigo');
           browser.assert.text('span#surname', 'Vespucci');
           browser.assert.elements('span#dates', 1);
+          assert.isNotNull(browser.site);
           done();
         });
       });
